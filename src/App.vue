7b88298ac4ca12856cc8706e77a9d44a745d786f
unknown <herrera05luis@gmail.com>
2022-03-31 00:08:38 -0400

    Country View Css

--- src/App.vue
+++ src/App.vue
@@ -47,12 +47,5 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: "Nunito Sans", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style src='@/assets/style.css'>
 </style>
--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,9 +1,57 @@
 <template>
-  <router-link to="/">VOLVER</router-link>
-  <h1>Coño de la madre</h1>
-  <h1>Vista de {{ name }}</h1>
-  <h1>Vista de {{ country.capital }}</h1>
-  <img :src="country.flag" alt="" />
+  <div class="country container">
+    <router-link class="country__back-btn" to="/">Back</router-link>
+    <div class="country__content">
+      <div class="country__flag">
+        <img :src="country.flag" alt="" />
+      </div>
+      <div class="country__info">
+        <h1 class="country__name">
+          {{ name }}
+        </h1>
+        <div class="country__meta">
+          <div class="country__meta-row">
+            <span class="country__meta-info"
+              ><b>Native Name:</b> {{ country.nativeName }}
+            </span>
+            <span class="country__meta-info"
+              ><b>Population:</b> {{ country.population }}
+            </span>
+            <span class="country__meta-info"
+              ><b>Region:</b> {{ country.region }}
+            </span>
+            <span class="country__meta-info"
+              ><b>Sub Region:</b> {{ country.subregion }}
+            </span>
+
+            <span class="country__meta-info"
+              ><b>Capital:</b> {{ country.capital }}
+            </span>
+          </div>
+          <div class="country__meta-row">
+            <span class="country__meta-info"
+              ><b>Top Level Domain:</b> {{ country.topLevelDomain[0] }}
+            </span>
+            <span class="country__meta-info"
+              ><b>Currencies:</b> {{ country.currencies.name }}
+            </span>
+            <span class="country__meta-info"
+              ><b>Languages: </b>
+              <template v-for="item in country.languages"
+                >{{ item.name }},
+              </template>
+            </span>
+          </div>
+        </div>
+        <div class="country__borders">
+          <span
+            ><b>Border Countries: </b>
+            <template v-for="item in country.borders">{{ item }}, </template>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,9 +68,13 @@ export default {
       countries.value.map((item) => {
         if (item.name === props.name) {
           country.value = item;
+          country.value.population = country.value.population
+            .toString()
+            .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
         }
       });
     }
+
     return {
       country,
     };
